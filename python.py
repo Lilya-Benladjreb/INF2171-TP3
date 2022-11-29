dea9bde0d63374c9580d4ef22ba71f811e223a75
Lilya Benladjreb <lilyacbenladjreb@gmail.com>
2022-11-29 01:00:02 -0500

    reading inout for txt

--- python.py
+++ python.py
@@ -5,10 +5,18 @@ def afficherMessageDebut():
     print(msgDim)
 
 
+
 def stockDonneJeu():
+    #aucune validation des données a faire
     fichier = open("input.txt","r")
-    lignes = fichier.readlines()
-    
+    input = []
+    for element in fichier:
+        input.append(element)
+
+    print(input)
+    #je ne cois pas que c'est le plus optimal, utiliser la pile et la file serait mieux
+    #resultat = ['7\n', '7\n', 'GBVVRVOBOOORBRBBO\n', 'BRGBBROVVOBGOORVV\n', 'GVGRGBOGBVRVBOB\n', 'hpnBnOnVnRnBnGnOnVnBnBnRnO\n']
+
 
 
 def entrerLignes():
@@ -16,7 +24,19 @@ def entrerLignes():
     print(msgLin)
     
 
+def entrerColonnes():
+    msgCol = "Entrer le nombre de colonnes de la grille\n"
+    print(msgCol)
+
+
+def afficheManuelCommandes():
+    msgAide = "Aide de Flood: \n h: Affiche le message courant \n p: Affiche la grille courante \n n: Jouer le prochain coup \n q: Quitter"
+    print(msgAide)
+
 
 if __name__ == "__main__":
     afficherMessageDebut()
     entrerLignes()
+    entrerColonnes()
+    stockDonneJeu()
+    
